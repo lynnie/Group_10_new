723d873c1b6ca365d16c6981c2705d7733c5059e
Alexander Styre <alexanderstyre@hotmail.com>
2015-05-07 09:58:57 +0200

    Added fields for entering Github repo.

--- app/src/main/java/com/mycompany/myfirstapp/DisplayEnterGithubActivity.java
+++ app/src/main/java/com/mycompany/myfirstapp/DisplayEnterGithubActivity.java
@@ -1,29 +1,29 @@
 package com.mycompany.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
 
 
-public class HelloGridView extends ActionBarActivity {
-    public final static String EXTRA_MESSAGE ="com.mycompany.myfirstapp.MESSAGE";
+public class DisplayEnterGithubActivity extends ActionBarActivity {
+
+    public static final String GITHUB_REPO = "com.mycompany.myfirstapp.GITHUB_REPO";
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_display_enter_github);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
+        getMenuInflater().inflate(R.menu.menu_display_enter_github, menu);
         return true;
     }
 
@@ -42,14 +42,15 @@ public class HelloGridView extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    public void displaySelectDeckOfCards(View view) {
-        Intent intent = new Intent(this, DisplaySelectDeckActivity.class);
+    public void getGithubRepository(View view) {
+        Intent intent = new Intent(this, DisplayGithubActivity.class);
+        EditText editText = (EditText) findViewById(R.id.github_url);
+        String githubURL = editText.getText().toString();
+        String commitInformation = readCommits(githubURL);
+        intent.putExtra(GITHUB_REPO, commitInformation);
         startActivity(intent);
     }
-
-    public void displayEnterGithub(View view) {
-        Intent intent = new Intent(this, DisplayEnterGithubActivity.class);
-        startActivity(intent);
+    public String readCommits(String githubURL){
+        return githubURL;
     }
 }
--- app/src/main/java/com/mycompany/myfirstapp/DisplayGithubActivity.java
+++ app/src/main/java/com/mycompany/myfirstapp/DisplayGithubActivity.java
@@ -1,29 +1,30 @@
 package com.mycompany.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class HelloGridView extends ActionBarActivity {
-    public final static String EXTRA_MESSAGE ="com.mycompany.myfirstapp.MESSAGE";
+public class DisplayGithubActivity extends ActionBarActivity {
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        Intent intent = getIntent();
+        String repo = intent.getStringExtra(DisplayEnterGithubActivity.GITHUB_REPO);
+        TextView textView = new TextView(this);
+        textView.setText(repo);
+        setContentView(textView);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
+        getMenuInflater().inflate(R.menu.menu_display_github, menu);
         return true;
     }
 
@@ -40,16 +41,5 @@ public class HelloGridView extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-
-    public void displaySelectDeckOfCards(View view) {
-        Intent intent = new Intent(this, DisplaySelectDeckActivity.class);
-        startActivity(intent);
-    }
-
-    public void displayEnterGithub(View view) {
-        Intent intent = new Intent(this, DisplayEnterGithubActivity.class);
-        startActivity(intent);
     }
 }
