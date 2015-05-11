475e5e27e8379d865e527be69b519302d48c8f7b
Alexander Styre <alexanderstyre@hotmail.com>
2015-05-11 17:28:02 +0200

    List of clickable buttons for repositories list.

--- app/src/main/java/com/mycompany/myfirstapp/DisplayGithubActivity.java
+++ app/src/main/java/com/mycompany/myfirstapp/DisplayGithubActivity.java
@@ -5,8 +5,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class DisplayGithubActivity extends ActionBarActivity {
@@ -16,9 +26,34 @@ public class DisplayGithubActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String listrepo = intent.getStringExtra(DisplayEnterGithubActivity.GITHUB_REPO);
-        TextView textView = new TextView(this);
-        textView.setText(listrepo);
-        setContentView(textView);
+        // TextView textView = new TextView(this);
+        // textView.setText(listrepo);
+        // setContentView(textView);
+        try {
+            JSONArray jsonArray = new JSONArray(listrepo);
+            GridLayout layout = new GridLayout(this);
+            GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams();
+            for(int i = 0; i < jsonArray.length(); i++) {
+                final Button button = new Button(this);
+                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+                button.setText(jsonObject.getString("full_name"));
+                final DisplayGithubActivity displayGithub = this;
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        TextView textView = new TextView(displayGithub);
+                        textView.setText(button.getText());
+                        setContentView(textView);
+//                        Intent intent = new Intent(displayGithub, DisplayEnterGithubActivity.class);
+//                        startActivity(intent);
+                    }
+                });
+                layout.addView(button);
+            }
+            setContentView(layout);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         // WebView webview = new WebView(this);
         // setContentView(webview);
         // for example, https://api.github.com/repos/lynnie/Group_10_new/branches/master
